ced228d2888620e9e07f9892fe1a743f6046b6f4
samisatti791 <127220777+samisatti791@users.noreply.github.com>
2024-09-03 21:09:42 +0500

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Satti on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
